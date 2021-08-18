682fdfe92060b22eebed47f2b6f256ba3f7cfe19
Madison2727 <89124113+Madison2727@users.noreply.github.com>
2021-08-18 12:50:38 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -28,14 +28,14 @@ async def start(bot, update):
             await update.reply_cached_media(
                 file_id,
                 quote=True,
-                caption = caption,
+                caption = "@Fairytales27",
                 parse_mode="html",
                 reply_markup=InlineKeyboardMarkup(
                     [
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    'fairytales', url="https://t.me/lyf_of_chaos"
                                 )
                         ]
                     ]
